71b239df41701e7a256af1f51bb5f2e594753e4b
ishikabhagat29 <124775497+ishikabhagat29@users.noreply.github.com>
2023-06-14 15:16:07 +0530

    Update dictusingclass.py
    dictusing

--- dictusingclass.py
+++ dictusingclass.py
@@ -9,33 +9,34 @@ class value():
     print("key for value is:",value)
     """ this function looks for the key in the dictionary"""
       
-""" this function adds up all the values of keys  in the dictionary"""
 
-def sum_values(a):
- try:
-   return sum(x.values())
- except Exception as e:
-   print(e)
-print(sum(x.values()))
+    def sum_values(a):
+        """ this function adds up all the values of keys  in the dictionary"""
+        try:
+         return sum(x.values())
+        except Exception as e:
+         print(e)
+    print(sum(x.values()))
 
-""" this function multiplies the value by a factor in the dictionary"""
-   
-def multiply_values(x,factor):
-  try:
-    for key in x:
+
+    def multiply_values(x,factor):
+     """ this function multiplies the value by a factor in the dictionary"""
+     try:
+        for key in x:
+            return x[key]
         x[key] = x[key]* factor
-        return x[key]
-  except Exception as e:
-    print(e)
-  print(x[key].values)
+     except Exception as e:
+        print(e)
+    print(x[key].values)
 
 
-  def new_values(x,new_data):
-    try:
-      x.update(new_data)
-    except Exception as e :
-      print(e)
-print(x)
+    def new_values(x,new_data):
+     """ this function adds new data in the dictionary"""
+     try:
+        x.update(new_data)
+     except Exception as e :
+        print(e)
+    print(x)
 
 
 x = {
