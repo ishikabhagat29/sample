502265d871a926dea25020fcc588e532d9da6d59
ishikabhagat29 <124775497+ishikabhagat29@users.noreply.github.com>
2023-06-14 15:07:57 +0530

    fastapi

---
+++ fastapi/schemas/user.py
@@ -0,0 +1,13 @@
+def userEntity(item) -> dict:
+    return {
+        "id" :str(item["_id"]),
+        "name" :item["name"],
+        "email" :item["email"],
+        "password":item["password"] 
+   }
+
+
+def usersEntity(entity) -> list:
+    """A function to give usersentity as a list
+        return: users item """
+    return [userEntity(item) for item in entity]
